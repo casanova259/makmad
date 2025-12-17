29c8fc203e833ce0fee4f98b1db3795ded756696
Manik Sharma <casanova270407@gmail.com>
2025-12-17 15:54:32 +0530

    made navbar still needs a lot of work

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <>
+            <div className="min-h-screen min-w-screen bg-gray-100">
+                <nav className="px-6 py-6 flex justify-between items-center">
+                    <div className="flex items-center gap-2">
+                        <div className="w-8 h-8 bg-yellow-400 rounded-full"></div>
+                        <span className="font-semibold text-black text-lg">MAK</span>
+                    </div>
+                    <div className="flex gap-12 text-sm">
+                        <a href="#" className="text-black font-medium">Home</a>
+                        <a href="#" className="no-underline text-gray-600 hover:text-gray-900">About</a>
+                        <a href="#" className="text-gray-600 hover:text-gray-900">Works</a>
+                        <a href="#" className="text-gray-600 hover:text-gray-900">Contact</a>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
+export default NavBar
---
+++ src/Components/Profilepic.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profilepic() {
+  return (
+    <div>
+        <div className='grid grid-cols-2'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Profilepic
